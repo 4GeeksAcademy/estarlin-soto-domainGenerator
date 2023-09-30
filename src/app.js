29370534bf3d62e1303400fb7602d2b18ed93518
estarlinsoto <141869663+estarlinsoto@users.noreply.github.com>
2023-09-30 00:10:28 +0000

    Donegit status

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  const domainGenerator = (arr1, arr2, arr3) => {
+    let domains = [".com", ".net", ".us"];
+
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr2.length; j++) {
+        for (let x = 0; x < arr3.length; x++) {
+          let domainHack = `${arr1[i]}${arr2[j]}${arr3[x]}`;
+
+          console.log(
+            `${domainHack.slice(0, -2)}.${domainHack[domainHack.length - 2]}${
+              domainHack[domainHack.length - 1]
+            }`
+          );
+          console.log(`${arr1[i]}${arr2[j]}${arr3[x]}${domains[0]}`);
+          console.log(`${arr1[i]}${arr2[j]}${arr3[x]}${domains[1]}`);
+          console.log(`${arr1[i]}${arr2[j]}${arr3[x]}${domains[2]}`);
+        }
+      }
+    }
+  };
+
+  domainGenerator(pronoun, adj, noun);
 };
